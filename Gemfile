6162f5b238184c34e2758c7ae2dd64bf73f05fcd
Benjamin Southworth <inthecompanyof@me.com>
2016-01-18 00:34:52 +0000

    gemfile updated to sprokets roll back

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
-gem 'sprockets-rails'
+gem 'sprockets-rails', '~>2.3.3'
 
 group :development, :test do
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-gem 'sprockets-rails'
 
 module ArtsClubEast
   class Application < Rails::Application
